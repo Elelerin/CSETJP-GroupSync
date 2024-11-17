7e586c403ffcd2f3607424080d69ce2023421e41
Elelerin <118294100+Elelerin@users.noreply.github.com>
2024-11-16 22:07:49 -0800

    Basic stuff. Still working on it. Been sick.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
+import { useState } from 'react';
+const customData = require('./customData.json');
+
+
 
 export default function Index() {
+
+  const[parsedValue, setParsedValue] = useState("crittern");
+
+  function cCV(){
+    const v = JSON.stringify(customData);
+    setParsedValue(v);
+  }
+
+
   return (
     <View
       style={{
@@ -9,7 +22,46 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      
+    <Tasks input = {parsedValue} />
+    <Betton onPress = {cCV}/>
+
+     
+      
     </View>
-  );
+  )
 }
+
+function newTask{
+  return (
+    <TextInput
+      placeholder = "Task Name"
+    />
+    <TextInput
+      placeholder = "Due Date"
+    >
+
+    </TextInput>
+
+  )
+}
+
+
+function Tasks({input}){
+  return (
+    <Text>
+      {input}
+    </Text>
+  )
+}
+
+
+function Betton({onPress}){
+  return(
+    <Button
+          title="Load Tasks"
+          onPress = {onPress}
+      />
+  )
+}
+
