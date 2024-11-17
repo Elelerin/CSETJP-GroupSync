77602b02f6a0c9dd8663618a93acbf60c2130a36
Elelerin <118294100+Elelerin@users.noreply.github.com>
2024-11-17 13:01:16 -0800

    Text pulls from boxes.

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,42 @@
-import { Text, View, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 import { useState } from 'react';
 const customData = require('./customData.json');
 
 
 
 export default function Index() {
+//INIT STATES
+  const[parsedValue, setParsedValue] = useState('');
+  const[descValue, setDescValue] = useState ('');
+  const[tNameValue, setTNameValue] = useState ('');
 
-  const[parsedValue, setParsedValue] = useState("crittern");
 
-  function cCV(){
+//BOILER FUNCTIONS
+  function loadTasksFunction(){
+    //Load Tasks from File
     const v = JSON.stringify(customData);
     setParsedValue(v);
   }
 
 
+  function saveTaskFunction(){
+    //Get info from text boxes
+    
+    //convert to equivalent SQL / whatever
+
+    //push to server
+
+
+
+    //TEST DATA BELOW THIS LINE
+    //____________________________________
+
+    console.log(descValue);
+    console.log(tNameValue);
+  }
+
+
+//MAKE VIEW / WHATEVER
   return (
     <View
       style={{
@@ -24,25 +47,46 @@ export default function Index() {
     >
       
     <Tasks input = {parsedValue} />
-    <Betton onPress = {cCV}/>
-
-     
-      
+    <Betton onPress = {loadTasksFunction}/>
+    <NewTask
+      _saveTaskFunction={saveTaskFunction}
+      _descValue={descValue}
+      _setDescValue={setDescValue}
+      _tName={tNameValue}
+      _setTName={setTNameValue}
+    />
+    
     </View>
   )
 }
 
-function newTask{
+function NewTask({_saveTaskFunction, 
+                  _descValue, _setDescValue,
+                  _tName, _setTName}){
   return (
-    <TextInput
-      placeholder = "Task Name"
-    />
-    <TextInput
-      placeholder = "Due Date"
-    >
+    <div>
 
-    </TextInput>
+      <TextInput
+        style = {styles.input}
+        placeholder="Task Name"
+        value = {_tName}
+        onChangeText={text => _setTName(text)}
+      />
 
+      <TextInput
+        style = {styles.input}
+        placeholder="Task Desc"
+        value = {_descValue}
+        onChangeText={text => _setDescValue(text)}
+      />
+
+     
+
+      <Button
+        title="Save Task"
+        onPress = {_saveTaskFunction}
+      />
+    </div>
   )
 }
 
@@ -65,3 +109,19 @@ function Betton({onPress}){
   )
 }
 
+function TaskList({TasksToDisplay}){
+
+}
+
+function TaskRow({Name, Date, Creator}){
+
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
