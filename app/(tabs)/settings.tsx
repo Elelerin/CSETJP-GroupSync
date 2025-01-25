985c967fb5b8bb9e58967a2ef22ef7b4be98657b
Elelerin <szlovak.ethan@gmail.com>
2025-01-24 22:24:31 -0800

    basic fetch api. throwing big error about how we're not authorized. fix cors header. or something. other than that, correctly interfacing.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import PillButton from '@/components/PillButton'
+
+//const url = "https://wpznuj8aa6.execute-api.us-east-2.amazonaws.com/LambdaTest"
 
 export default function Settings() {
+
   return (
+    
+
     <View style={styles.container}>
       <Text>Hello world!</Text>
+      <PillButton icon="key" onPress={registerUser("doro", "dororo", "dorororo")}/>
     </View>
+
   );
+}
+
+function registerUser(userID, username, password){
+  return async () => {
+      try{
+      const response = await fetch(upurl, {
+      method : 'POST',
+      headers: {
+        'userID' : userID,
+        'username' : username,
+        'pword' : password 
+      },
+      body : 'NULL'
+      })
+
+      if(!response.ok){
+        throw new Error("USER CREATION ERROR");
+      }
+
+      const json = await response.json();
+      console.log(json);
+      return json;
+    }catch{
+      throw new Error("GENERAL ERROR USER CREATION");
+    }
+  }
 }
 
 const styles = StyleSheet.create({
