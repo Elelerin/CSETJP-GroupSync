06c6753603617ace5ae2e28b8ee3fffe738c55d5
Elelerin <szlovak.ethan@gmail.com>
2025-01-30 15:53:41 -0800

    task gathering function properly works now. settings page is a mess. will clean next.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,7 +1,9 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 import PillButton from '@/components/PillButton'
+import { PureComponent } from 'react';
 
-var url = "https://bxgjv0771m.execute-api.us-east-2.amazonaws.com/groupsync/User"
+var UserURL = "https://bxgjv0771m.execute-api.us-east-2.amazonaws.com/groupsync/User"
+var TaskURL = "https://bxgjv0771m.execute-api.us-east-2.amazonaws.com/groupsync/TaskFunction"
 export default function Settings() {
 
   return (
@@ -9,7 +11,9 @@ export default function Settings() {
 
     <View style={styles.container}>
       <Text>Hello world!</Text>
-      <PillButton icon="key" onPress={registerUser("doro", "dororo", "dorororo")}/>
+      <PillButton icon="key" onPress={registerUser("doro", "doro", "I<3DORO")}/>
+      <PillButton icon="key" onPress={registerTask("doro", "doro", "doro")}/>
+      <PillButton icon="key" onPress={getTask("doro")}/>
     </View>
 
   );
@@ -18,7 +22,7 @@ export default function Settings() {
 function registerUser(_userID, _username, _password){ 
   return async () => {
     try{
-      const response = await fetch(url, {
+      const response = await fetch(UserURL, {
         method : 'POST',
         body: JSON.stringify({
           userID : _userID,
@@ -40,27 +44,91 @@ function registerUser(_userID, _username, _password){
   }
 }
 
-  function getUser(_userID){ 
-    return async () => {
-      try{
-        const response = await fetch(url, {
-          method : 'POST',
-          body: JSON.stringify({
-            userID : _userID 
-          })
+function registerTask(_taskName, _taskDesc, _taskAuthor){ 
+  return async () => {
+    try{
+      const response = await fetch(TaskURL, {
+        method : 'POST',
+        body: JSON.stringify({
+          taskName : _taskName,
+          taskDesc : _taskDesc,
+          taskAuthor : _taskAuthor
         })
-  
-        if(!response.ok){
-          throw new Error("User retreival error");
-        }
-  
-        const json = response;
-        console.log(response);
-        return json;
-      }catch{
-  
+      })
+
+      if(!response.ok){
+        throw new Error("USER CREATION ERROR");
       }
+
+      const json = response;
+      console.log(response);
+      return json;
+    }catch{
+
     }
+  }
+}
+
+function getUser(_userID){ 
+  return async () => {
+    try{
+      const response = await fetch(UserURL, {
+        method : 'GET',
+        body: JSON.stringify({
+          userID : _userID 
+        })
+      })
+
+      if(!response.ok){
+        throw new Error("User retreival error");
+      }
+
+      const json = response;
+      console.log(response);
+      return json;
+    }catch{
+
+    }
+  }
+}
+
+function getTask(_taskAuthor){
+  return async () => {
+    try{
+      const response = await fetch(TaskURL, {
+        method : 'GET',
+        headers : {
+          taskAuthor : _taskAuthor
+        }
+      }).then((response) => {
+        const reader = response.body.getReader();
+        return new ReadableStream({
+          start(controller){
+            return pump();
+            function pump(){
+              return reader.read().then(({done, value}) =>{
+                if(done){
+                  controller.close();
+                  return;
+                }
+                controller.enqueue(value);
+                return pump();
+              })
+            }
+          }
+        })
+      })
+      .then((stream) => new Response(stream))
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+
+      console.log(response);
+      console.log("TEST");
+      return response;
+    }catch{
+        throw "Darn, response retrieval error";
+    }
+  }
 }
 
 const styles = StyleSheet.create({
