22f3914ce332b494a36f280620b9400ef4effcd3
Joe Williams <joe.williams@oit.edu>
2025-01-31 11:47:28 -0800

    started work on group page

--- app/group/[id].tsx
+++ app/group/[id].tsx
@@ -1,12 +1,26 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
 import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function GroupHome() {
   const { id } = useLocalSearchParams();
 
   return (
-    <View>
+    <View style={styles.pageContainer}>
       <Text>Details for group {id}.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: useThemeColor("backgroundPrimary"),
+    paddingHorizontal: 20,
+    paddingTop: 10,
+  },
+  usersCard: {
+    
+  }
+});
