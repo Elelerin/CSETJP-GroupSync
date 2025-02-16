032cbda0bee78632b174b2aeef23991e755633c2
MujtabaAliProjects <mujtaba.i1@yahoo.com>
2025-02-15 22:43:57 -0800

    Sort By Button Added, but it replaced the + task button. Will push new fix

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -1,28 +1,33 @@
 import { FlatList, View, StyleSheet } from "react-native";
 import { useCallback, useState } from 'react';
-import { IconButton } from "react-native-paper";
 
 import * as Tasks from "@/services/tasks";
 import PillButton from '@/components/PillButton'
-import { TaskView }  from "@/components/TaskView";
+import {TaskView}  from "@/components/TaskView";
 import { useThemeColor } from "@/hooks/useThemeColor";
-import TaskCreationModal from "@/components/TaskCreationModal";
 
+import { Menu, Button } from "react-native-paper";
 
 export default function Index() {
   const [selectedTasks, setSelectedTasks] = useState<number[]>([]);
   const [tasks, setTasks] = useState<Tasks.Task[]>([]);
-  const [modalVisible, setModalVisible] = useState(false);
-
+  //sort by button
+  const [sortBy, setSortBy] = useState<"name" | "date" | "size">("name");
+  const [menuVisible, setMenuVisible] = useState(false);
   var User = 'doro';
   const TaskURL = "https://bxgjv0771m.execute-api.us-east-2.amazonaws.com/groupsync/TaskFunction"
 
   //TODO: Remove nested 'then' chain-hell. 
-  
-  // const onLoad = async () => {
-  //   getTasks(User);
-  //   setTasks(await Tasks.getTasks());
-  // }
+  const sortedTasks = [...tasks].sort((a, b) => {
+    if (sortBy === "name") return a.title.localeCompare(b.title);
+    if (sortBy === "date") return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
+    if (sortBy === "size") return (a.description?.length || 0) - (b.description?.length || 0);
+    return 0;
+  });
+  const onLoad = async () => {
+    getTasks(User);
+    setTasks(await Tasks.getTasks());
+  }
 
   function clearTasks() {
     setTasks([]);
@@ -42,42 +47,6 @@ export default function Index() {
   
     return taskToAdd;
   }
-
-  function registerTask(inputTask : Tasks.Task, userID : string){ 
-    return async () => {
-      try{
-        let dateToParse = inputTask.dueDate;
-              const pad = (num : Number) => num.toString().padStart(2, '0');
-              const toSend = `${pad(dateToParse.getFullYear())}-${pad(dateToParse.getMonth() + 1)}-${pad(dateToParse.getDate())} ${pad(dateToParse.getHours())}:${pad(dateToParse.getMinutes())}:${pad(dateToParse.getSeconds())}`;
-              console.log(toSend);
-        console.log(toSend);
-  
-        let fetchBody = {
-          taskName : inputTask.title,
-          taskDesc : inputTask.description,
-          taskAuthor : userID,
-          ...(inputTask.dueDate && {dueDate : dateToParse})
-        }
-        
-        console.log(fetchBody);
-        const response = await fetch(TaskURL, {
-          method : 'POST',
-          body: JSON.stringify(fetchBody)
-        });
-  
-        if(!response.ok){
-          throw new Error("Error registering task.");
-        }
-  
-        const json = response;
-        console.log(response);
-        return json;
-      }catch{
-  
-      }
-    }
-  }
-
 
   function getTasks(_taskAuthor: string){
     var toReturn = "ERROR";
@@ -125,23 +94,26 @@ export default function Index() {
       }catch{
           throw "Darn, response retrieval error";
       }
-    }
+    };
   }
   
 
 //Return render of tasks page
   return (
     <View style={styles.container}>
-
-      <TaskCreationModal modalVisible={modalVisible} setModalVisible={setModalVisible} />
-      
-      <IconButton
-        icon={"note-plus"}
-        iconColor={useThemeColor("textSecondary")}
-        size={36}
-        onPress={() => { setModalVisible(true); }}
-      />
-      <PillButton icon={"download"} onPress={getTasks(User)}/>
+      <Menu
+      visible={menuVisible}
+      onDismiss={() => setMenuVisible(false)}
+      anchor={
+        <Button mode="contained" onPress={() => setMenuVisible(true)}>
+          Sort By
+        </Button>
+      }
+    >
+      <Menu.Item onPress={() => setSortBy("name")} title="Name" />
+      <Menu.Item onPress={() => setSortBy("date")} title="Date" />
+      <Menu.Item onPress={() => setSortBy("size")} title="Size" />
+    </Menu>
       <PillButton icon={"download"} onPress={getTasks(User)}/>
       <PillButton icon={"trash"} onPress={clearTasks}/>
       <FlatList 
@@ -162,6 +134,12 @@ export default function Index() {
     </View>
   )
 }
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
