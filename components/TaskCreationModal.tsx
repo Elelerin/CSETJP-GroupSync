090dc7283eae87a92487f4d4894f8f1f9e3d3217
Joe Williams <joe.williams@oit.edu>
2025-02-15 17:33:42 -0800

    added date picker to task creator

--- components/TaskCreationModal.tsx
+++ components/TaskCreationModal.tsx
@@ -1,7 +1,8 @@
 import { useThemeColor } from "@/hooks/useThemeColor";
 import React from "react";
 import { Modal, View, StyleSheet, Text } from "react-native";
-import { IconButton, TextInput } from "react-native-paper";
+import { IconButton, TextInput, Button, Icon } from "react-native-paper";
+import { DatePickerInput } from "react-native-paper-dates";
 
 interface Props {
   modalVisible: boolean,
@@ -9,7 +10,7 @@ interface Props {
 }
 
 export default function TaskCreationModal({ modalVisible, setModalVisible }: Props) {
-  const containerStyle = {backgroundColor: 'white', padding: 20};
+  const [dueDate, setDueDate] = React.useState<Date>();
 
   return (
     <Modal
@@ -19,6 +20,7 @@ export default function TaskCreationModal({ modalVisible, setModalVisible }: Pro
       <View style={styles.centeredView}>
         <View style={styles.container}>
           <Text style={styles.textTitle}>Create Task</Text>
+          {/* task name */}
           <View style={styles.textInputContainer}>
             <TextInput
               label="Name"
@@ -26,6 +28,7 @@ export default function TaskCreationModal({ modalVisible, setModalVisible }: Pro
               textColor={useThemeColor("textPrimary")}
             />
           </View>
+          {/* description */}
           <View style={styles.textInputContainer}>
             <TextInput
               label="Description"
@@ -34,18 +37,28 @@ export default function TaskCreationModal({ modalVisible, setModalVisible }: Pro
               textAlignVertical={"top"}
             />
           </View>
-          <View style={{ flexDirection: "row", marginTop: 10 }}>
+          {/* date */}
+          <DatePickerInput            
+            style={styles.datePicker}
+            locale="en"
+            label="Due Date"
+            value={dueDate}
+            onChange={(d) => setDueDate(d)}
+            inputMode="start"
+          />
+          {/* submit/close buttons */}
+          <View style={{ flexDirection: "row" }}>
             <IconButton
-              icon={"close-thick"}
-              iconColor={useThemeColor("backgroundSecondary")}
-              style={styles.iconButton}
+              icon={"close"}
+              iconColor={useThemeColor("highlight")}
+              style={styles.submitButton}
               size={36}
               onPress={() => setModalVisible(false)}
             />
             <IconButton
-              icon={"check-bold"}
-              iconColor={useThemeColor("backgroundSecondary")}
-              style={styles.iconButton}
+              icon={"check"}
+              iconColor={useThemeColor("highlight")}
+              style={styles.submitButton}
               size={36}
               onPress={() => {
                 console.log("Adding task...");
@@ -79,20 +92,29 @@ const styles = StyleSheet.create({
     color: useThemeColor("textPrimary"),
     fontWeight: 600,
     fontSize: 36,
-    marginBottom: 0
+    marginBottom: -10
   },
   textInputContainer: {
     backgroundColor: useThemeColor("backgroundSecondary"),
-    marginVertical: 2,
+    marginVertical: 10,
     width: "100%",
     minHeight: 50,
     textAlignVertical: "top"
   },
   textInput: {
     paddingHorizontal: 0,
-    backgroundColor: "transparent"
+    backgroundColor: "transparent",
+    fontSize: 16,
+    fontWeight: 400,
   },
-  iconButton: {
-    backgroundColor: useThemeColor("highlight"),
-  }
+  // also used for the close button
+  submitButton: {
+    backgroundColor: useThemeColor("backgroundSecondary"),
+  },
+  datePicker: {
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: "transparent",
+    paddingHorizontal: 0
+  },
 });
