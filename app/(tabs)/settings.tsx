dc9cfd55e5b7a18a251a49918d8b96982dfc6bd1
Elelerin <szlovak.ethan@gmail.com>
2025-02-23 17:45:14 -0800

    added basic function for pulling group users. incomplete, somewhat, but the backend pulls users. do not push to the thing yet. that functino will be added shortly.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -121,6 +121,36 @@ function getUser(_userID: string) {
 }
 
 
+/**
+ * Gets list of users in database as an array of userIDs
+ * Will change to be DISPLAYNAMES, but that's all backend stuff. For now, if this is loaded in
+ * then you won't need to change anything when I push it. 
+ * @param _userID Should this be a number?
+ */
+function getGroupUsers(groupID: number) { 
+  return async () => {
+    try {
+      const response = await fetch(UserURL, {
+        method : 'GET',
+        headers : {
+          'usergroupgroup' : groupID.toString();
+        }
+      });
+
+      if(!response.ok){
+        throw new Error("Group Users Retrieval error");
+      }
+
+      const json = response;
+      console.log(response);
+      return json;
+    } catch {
+
+    }
+  }
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
