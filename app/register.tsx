036138033318c97f57796cd8be4fa91a9bd2950d
Joe Williams <hey-there-joe@outlook.com>
2025-05-15 16:31:21 -0700

    finalized registration screen

--- app/register.tsx
+++ app/register.tsx
@@ -1,17 +1,135 @@
 // app/register.jsx
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View, Alert, StyleSheet } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
 import { RelativePathString, useRouter } from "expo-router";
 import { registerUser } from "../services/firebaseAuthService";
+import { useThemeColor, getThemeObject } from "@/hooks/useThemeColor";
+
+// conditions for registering
+const accountRequirements = {
+  phoneNumberRequired: true,
+  // requirements for passwords
+  password: {
+    minLength: 6, // 0 to remove the requirement
+    mixedCase: true, // password must contain uppercase and lowercase characters
+    containsNumber: true,
+    containsSpecialCharacter: true
+  }
+};
+
+let invalidPasswordMessage = "Password must ";
+if (accountRequirements.password.minLength > 0) {
+  invalidPasswordMessage += `be at least ${accountRequirements.password.minLength} characters`
+}
+
+let buffer: string[] = [];
+if (accountRequirements.password.mixedCase) {
+  buffer.push("uppercase and lowercase letters");
+}
+if (accountRequirements.password.containsNumber) {
+  buffer.push("a number");
+}
+if (accountRequirements.password.containsSpecialCharacter) {
+  buffer.push("a special character");
+}
+
+if (buffer.length > 0) {
+  invalidPasswordMessage += " and contain ";
+}
+if (buffer.length === 1) {
+  invalidPasswordMessage += `${buffer[0]}`;
+}
+else if (buffer.length === 2) {
+  invalidPasswordMessage += `${buffer[0]} and ${buffer[1]}`;
+}
+else if (buffer.length === 3) {
+  invalidPasswordMessage += `${buffer[0]}, ${buffer[1]}, and ${buffer[2]}`;
+}
+
+invalidPasswordMessage += ".";
+
+// messages displayed at the bottom for each issue. if there are multiple issues, the message that
+// is highest in this list is used
+const errorMessages = {
+  noUsername: "A username is required.",
+  noEmail: "An email address is required.", // used when email is missing entirely
+  invalidEmail: "Email address is invalid.",
+  noPhoneNumber: "A phone number is required.", // used when phone number is missing entirely
+  invalidPhoneNumber: "Phone number is invalid.",
+  noPassword: "A password is required.", // used when password is missing entirely
+  invalidPassword: invalidPasswordMessage, // used when password doesn't meet all requirements
+  passwordsDoNotMatch: "Passwords do not match."
+};
 
 export default function RegisterScreen() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [loading, setLoading] = useState(false);
+  const [submitButtonEnabled, setSubmitButtonEnabled] = useState(false);
+  const [currentErrorMessage, setCurrentErrorMessage] = useState(errorMessages.noUsername);
+
+  // updates whether fields are enabled and returns what error message should be displayed
+  function updateFieldStatus(): string {
+    // assume registration is disabled
+    setSubmitButtonEnabled(false);
+
+    if (username === "") {
+      return errorMessages.noUsername;
+    }
+
+    if (email === "") {
+      return errorMessages.noEmail;
+    }
+    // this only checks if the email COULD exist, not if it actually does
+    if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))) {
+      return errorMessages.invalidEmail;
+    }
+
+    if (phoneNumber === "" && accountRequirements.phoneNumberRequired) {
+      return errorMessages.noPhoneNumber;
+    }
+    if (phoneNumber !== "" && !(/\+?[0-9]{3,5}\-[0-9]{3}-[0-9]{4}$/.test(phoneNumber))) {
+      return errorMessages.invalidPhoneNumber;
+    }
+
+    if (password === "") {
+      return errorMessages.noPassword;
+    }
+
+    // check if all password requirements are met
+    let passwordValid = true;
+    if (password.length < accountRequirements.password.minLength) {
+      passwordValid = false;
+    }
+    if (passwordValid && accountRequirements.password.mixedCase) {
+      passwordValid = /[a-z]/.test(password) && /[A-Z]/.test(password);
+    }
+    if (passwordValid && accountRequirements.password.containsNumber) {
+      passwordValid = /\d/.test(password);
+    }
+    if (passwordValid && accountRequirements.password.containsSpecialCharacter) {
+      passwordValid = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password);
+    }
+    if (!passwordValid) {
+      return errorMessages.invalidPassword;
+    }
+
+    if (password !== password2) {
+      return errorMessages.passwordsDoNotMatch;
+    }
+
+    setSubmitButtonEnabled(true);
+    return "";
+  }
+
+  useEffect(() => {
+    setCurrentErrorMessage(updateFieldStatus);
+  }, [username, email, phoneNumber, password, password2]);
 
   const handleRegister = async () => {
     if (!username || !email || !password) {
@@ -36,15 +154,17 @@ export default function RegisterScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Create an Account</Text>
+      <Text style={styles.title}>Create Account</Text>
 
       <View style={styles.inputContainer}>
-      <TextInput
+        <TextInput
           label="Username"
           value={username}
           onChangeText={setUsername}
           style={styles.input}
           autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
         />
         <TextInput
           label="Email"
@@ -52,6 +172,17 @@ export default function RegisterScreen() {
           onChangeText={setEmail}
           style={styles.input}
           autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
+        />
+        <TextInput
+          label="Phone Number"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          style={styles.input}
+          autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
         />
         <TextInput
           label="Password"
@@ -60,6 +191,8 @@ export default function RegisterScreen() {
           secureTextEntry
           style={styles.input}
           autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
         />
         <TextInput
           label="Reenter Password"
@@ -68,6 +201,8 @@ export default function RegisterScreen() {
           secureTextEntry
           style={styles.input}
           autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
         />
 
         <Button
@@ -75,6 +210,8 @@ export default function RegisterScreen() {
           onPress={()=>{}}
           loading={loading}
           style={styles.button}
+          theme={submitButtonTheme}
+          disabled={!submitButtonEnabled}
         >
           {loading ? "Processing..." : "Create Account"}
         </Button>
@@ -87,10 +224,22 @@ export default function RegisterScreen() {
         >
           Login With Existing Account
         </Button>
+
+        <Text style={styles.errorText}>{currentErrorMessage}</Text>
       </View>
     </View>
   );
 }
+
+// for correct colors when the submit button is disabled
+const submitButtonTheme = {
+  ...getThemeObject(),
+  colors: {
+    ...getThemeObject().colors,
+    surfaceDisabled: useThemeColor("surface"),
+    onSurfaceDisabled: useThemeColor("surfaceVariant")
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -105,12 +254,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
     marginBottom: 100,
-    textAlign: "center",
-  },
-  subtitle: {
-    fontSize: 14,
-    color: "#ccc",
-    marginBottom: 20,
     textAlign: "center",
   },
   inputContainer: {
@@ -129,6 +272,12 @@ const styles = StyleSheet.create({
   whiteButton: {
     borderColor: "#fff",
   },
+  errorText: {
+    fontSize: 16,
+    color: "#ff4040",
+    textAlign: "center",
+    marginTop: 10,
+  }
 });
 
 // const styles = {
